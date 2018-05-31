b936ea580b82154b73e971fe9a4d61496dbf404d
lontkovsky <lontkovsky@gmail.com>
2018-05-31 16:32:47 +0300

    add styles and pare logic

--- vueApp.js
+++ vueApp.js
@@ -3,21 +3,42 @@ const url = "https://www.binance.com/exchange/public/product";
 const vm = new Vue({
     el: '#app',
     data: {
-        symbols: {}
-           },
+        x1: {},
+        x2: {}
+
+        },
     mounted() {
-        this.upDate();
-        this.timer = setInterval(this.upDate, 900000)
-    },
+        this.upDateFirst();
+        this.upDateSec();
+        this.timerFirst = setInterval(this.upDateFirst, 900000);
+        this.timerSec = setInterval(this.upDateSec, 10000);
+        },
 
     methods:{
-        upDate: function() {
-           axios.get(url).then(response => {
-                this.symbols = response.data['data']
+        upDateFirst: function() {
+           axios.get(url).then(response1 => {
+                this.x1 = response1.data['data']
                 })
         },
-        cancelAutoUpdate: function() { clearInterval(this.timer) },
 
-        beforeDestroy() { clearInterval(this.timer)}
+        upDateSec: function() {
+            axios.get(url).then(response2 => {
+                this.x2 = response2.data['data'];
+                console.log(response2.data['data']['1']['tradedMoney'])
+            })
+        },
+
+
+
+        cancelAutoUpdate: function() {
+
+            clearInterval(this.timerFirst)
+            clearInterval(this.timerSec)
+        },
+
+        beforeDestroy() {
+           clearInterval(this.timerFirst);
+           clearInterval(this.timerSec);
+        }
     }
 });
