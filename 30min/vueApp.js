19ac3f23a58a0361baac3874cab9fa0d4f855927
lontkovsky <lontkovsky@gmail.com>
2018-05-31 17:48:05 +0300

    add 15/30/60 min and 1day pages

---
+++ 30min/vueApp.js
@@ -0,0 +1,44 @@
+const url = "https://www.binance.com/exchange/public/product";
+
+const vm = new Vue({
+    el: '#app',
+    data: {
+        x1: {},
+        x2: {}
+
+        },
+    mounted() {
+        this.upDateFirst();
+        this.upDateSec();
+        this.timerFirst = setInterval(this.upDateFirst, 1800000);
+        this.timerSec = setInterval(this.upDateSec, 10000);
+        },
+
+    methods:{
+        upDateFirst: function() {
+           axios.get(url).then(response1 => {
+                this.x1 = response1.data['data']
+                })
+        },
+
+        upDateSec: function() {
+            axios.get(url).then(response2 => {
+                this.x2 = response2.data['data'];
+                console.log(response2.data['data']['1']['tradedMoney'])
+            })
+        },
+
+
+
+        cancelAutoUpdate: function() {
+
+            clearInterval(this.timerFirst)
+            clearInterval(this.timerSec)
+        },
+
+        beforeDestroy() {
+           clearInterval(this.timerFirst);
+           clearInterval(this.timerSec);
+        }
+    }
+});
